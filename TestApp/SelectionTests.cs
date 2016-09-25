bb6bed46f6df20798d88645bd0cb5701a3228795
Yeudakimenka-Maksim <evdokimenko.maxim@gmail.com>
2016-09-25 21:34:43 +0300

    Task 1

---
+++ TestApp/SelectionTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ORM.EF.Context;
+
+namespace TestApp
+{
+  [TestClass]
+  public class SelectionTests
+  {
+    [TestMethod]
+    public void GetOrdersHavingProductsOfSpecificCategory()
+    {
+      using (var db = new NorthwindContext())
+      {
+        var categoryId = db.Categories.First().CategoryID;
+        var orders = db.Orders.Where(o => o.Order_Details.Any(d => d.Product.Category.CategoryID == categoryId));
+
+        foreach (var order in orders)
+        {
+          Console.WriteLine($"Order id: {order.OrderID} | Customer: {order.Customer.CompanyName}");
+
+          foreach (var orderDetail in order.Order_Details)
+          {
+            Console.WriteLine($"Product: {orderDetail.Product.ProductName}");
+          }
+
+          Console.WriteLine();
+        }
+      }
+    }
+  }
+}
