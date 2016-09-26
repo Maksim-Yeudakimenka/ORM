0298c66623931bb5c28e6cbae02447d68c2685a4
Yeudakimenka-Maksim <evdokimenko.maxim@gmail.com>
2016-09-26 23:03:14 +0300

    Task 3

--- ORM.EF/Migrations/Configuration.cs
+++ ORM.EF/Migrations/Configuration.cs
@@ -1,3 +1,5 @@
+using ORM.EF.Entities;
+
 namespace ORM.EF.Migrations
 {
     using System;
@@ -17,15 +19,81 @@ namespace ORM.EF.Migrations
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            //  to avoid creating duplicate seed data.
+
+            context.Categories.AddOrUpdate(
+                c => c.CategoryID,
+                new Category { CategoryID = 1, CategoryName = "Beverages", Description = "Soft drinks, coffees, teas, beers, and ales" },
+                new Category { CategoryID = 2, CategoryName = "Condiments", Description = "Sweet and savory sauces, relishes, spreads, and seasonings" },
+                new Category { CategoryID = 3, CategoryName = "Confections", Description = "Desserts, candies, and sweet breads" },
+                new Category { CategoryID = 4, CategoryName = "Dairy Products", Description = "Cheeses" },
+                new Category { CategoryID = 5, CategoryName = "Grains/Cereals", Description = "Breads, crackers, pasta, and cereal" },
+                new Category { CategoryID = 6, CategoryName = "Meat/Poultry", Description = "Prepared meats" },
+                new Category { CategoryID = 7, CategoryName = "Produce", Description = "Dried fruit and bean curd" },
+                new Category { CategoryID = 8, CategoryName = "Seafood", Description = "Seaweed and fish" });
+
+            context.Regions.AddOrUpdate(
+                r => r.RegionID,
+                new Region { RegionID = 1, RegionDescription = "Eastern" },
+                new Region { RegionID = 2, RegionDescription = "Western" },
+                new Region { RegionID = 3, RegionDescription = "Northern" },
+                new Region { RegionID = 4, RegionDescription = "Southern" });
+
+            context.Territories.AddOrUpdate(
+                t => t.TerritoryID,
+                new Territory { TerritoryID = "01581", TerritoryDescription = "Westboro", RegionID = 1 },
+                new Territory { TerritoryID = "01730", TerritoryDescription = "Bedford", RegionID = 1 },
+                new Territory { TerritoryID = "01833", TerritoryDescription = "Georgetow", RegionID = 1 },
+                new Territory { TerritoryID = "02116", TerritoryDescription = "Boston", RegionID = 1 },
+                new Territory { TerritoryID = "02139", TerritoryDescription = "Cambridge", RegionID = 1 },
+                new Territory { TerritoryID = "02184", TerritoryDescription = "Braintree", RegionID = 1 },
+                new Territory { TerritoryID = "02903", TerritoryDescription = "Providence", RegionID = 1 },
+                new Territory { TerritoryID = "03049", TerritoryDescription = "Hollis", RegionID = 3 },
+                new Territory { TerritoryID = "03801", TerritoryDescription = "Portsmouth", RegionID = 3 },
+                new Territory { TerritoryID = "06897", TerritoryDescription = "Wilton", RegionID = 1 },
+                new Territory { TerritoryID = "07960", TerritoryDescription = "Morristown", RegionID = 1 },
+                new Territory { TerritoryID = "08837", TerritoryDescription = "Edison", RegionID = 1 },
+                new Territory { TerritoryID = "10019", TerritoryDescription = "New York", RegionID = 1 },
+                new Territory { TerritoryID = "10038", TerritoryDescription = "New York", RegionID = 1 },
+                new Territory { TerritoryID = "11747", TerritoryDescription = "Mellvile", RegionID = 1 },
+                new Territory { TerritoryID = "14450", TerritoryDescription = "Fairport", RegionID = 1 },
+                new Territory { TerritoryID = "19428", TerritoryDescription = "Philadelphia", RegionID = 3 },
+                new Territory { TerritoryID = "19713", TerritoryDescription = "Neward", RegionID = 1 },
+                new Territory { TerritoryID = "20852", TerritoryDescription = "Rockville", RegionID = 1 },
+                new Territory { TerritoryID = "27403", TerritoryDescription = "Greensboro", RegionID = 1 },
+                new Territory { TerritoryID = "27511", TerritoryDescription = "Cary", RegionID = 1 },
+                new Territory { TerritoryID = "29202", TerritoryDescription = "Columbia", RegionID = 4 },
+                new Territory { TerritoryID = "30346", TerritoryDescription = "Atlanta", RegionID = 4 },
+                new Territory { TerritoryID = "31406", TerritoryDescription = "Savannah", RegionID = 4 },
+                new Territory { TerritoryID = "32859", TerritoryDescription = "Orlando", RegionID = 4 },
+                new Territory { TerritoryID = "33607", TerritoryDescription = "Tampa", RegionID = 4 },
+                new Territory { TerritoryID = "40222", TerritoryDescription = "Louisville", RegionID = 1 },
+                new Territory { TerritoryID = "44122", TerritoryDescription = "Beachwood", RegionID = 3 },
+                new Territory { TerritoryID = "45839", TerritoryDescription = "Findlay", RegionID = 3 },
+                new Territory { TerritoryID = "48075", TerritoryDescription = "Southfield", RegionID = 3 },
+                new Territory { TerritoryID = "48084", TerritoryDescription = "Troy", RegionID = 3 },
+                new Territory { TerritoryID = "48304", TerritoryDescription = "Bloomfield Hills", RegionID = 3 },
+                new Territory { TerritoryID = "53404", TerritoryDescription = "Racine", RegionID = 3 },
+                new Territory { TerritoryID = "55113", TerritoryDescription = "Roseville", RegionID = 3 },
+                new Territory { TerritoryID = "55439", TerritoryDescription = "Minneapolis", RegionID = 3 },
+                new Territory { TerritoryID = "60179", TerritoryDescription = "Hoffman Estates", RegionID = 2 },
+                new Territory { TerritoryID = "60601", TerritoryDescription = "Chicago", RegionID = 2 },
+                new Territory { TerritoryID = "72716", TerritoryDescription = "Bentonville", RegionID = 4 },
+                new Territory { TerritoryID = "75234", TerritoryDescription = "Dallas", RegionID = 4 },
+                new Territory { TerritoryID = "78759", TerritoryDescription = "Austin", RegionID = 4 },
+                new Territory { TerritoryID = "80202", TerritoryDescription = "Denver", RegionID = 2 },
+                new Territory { TerritoryID = "80909", TerritoryDescription = "Colorado Springs", RegionID = 2 },
+                new Territory { TerritoryID = "85014", TerritoryDescription = "Phoenix", RegionID = 2 },
+                new Territory { TerritoryID = "85251", TerritoryDescription = "Scottsdale", RegionID = 2 },
+                new Territory { TerritoryID = "90405", TerritoryDescription = "Santa Monica", RegionID = 2 },
+                new Territory { TerritoryID = "94025", TerritoryDescription = "Menlo Park", RegionID = 2 },
+                new Territory { TerritoryID = "94105", TerritoryDescription = "San Francisco", RegionID = 2 },
+                new Territory { TerritoryID = "95008", TerritoryDescription = "Campbell", RegionID = 2 },
+                new Territory { TerritoryID = "95054", TerritoryDescription = "Santa Clara", RegionID = 2 },
+                new Territory { TerritoryID = "95060", TerritoryDescription = "Santa Cruz", RegionID = 2 },
+                new Territory { TerritoryID = "98004", TerritoryDescription = "Bellevue", RegionID = 2 },
+                new Territory { TerritoryID = "98052", TerritoryDescription = "Redmond", RegionID = 2 },
+                new Territory { TerritoryID = "98104", TerritoryDescription = "Seattle", RegionID = 2 });
         }
     }
 }
